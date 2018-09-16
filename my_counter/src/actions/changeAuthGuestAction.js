fcb8a624822477d85adf3e6d1091447253b64433
umbrella121 <31348828+umbrella121@users.noreply.github.com>
2018-09-16 16:37:32 +0300

    remove counter

--- my_counter/src/actions/changeAuthGuestAction.js
+++
@@ -1,8 +0,0 @@
-import { IS_GUEST_ENTER } from '../constants/Page.js'
-
-export const changeAuth = (payload) => {
-	return {
-		type: IS_GUEST_ENTER,
-		payload
-	}
-}
--- my_counter/src/func/getKey.js
+++
@@ -1,9 +0,0 @@
-	const getKey = (str) => {
-		let key = 0;
-		for (let i = 0; i < str.length; i++) {
-			key += str.charCodeAt(i);
-		}
-		return key.toString();
-	}
-
-export default getKey
